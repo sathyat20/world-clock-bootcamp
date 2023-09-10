c0b16a384cdb448eaa6fbf1e43dc49525d70903a
sathyat20 <sathya.thenappan.23@dartmouth.edu>
2023-09-10 17:55:11 +0800

    world clock sathya

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";  
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Container fluid="md">
+            <Row className="bg-primary m-5 p-5">
+              <Col>City</Col>
+              <Col>Clock</Col>
+            </Row>
+            <Row>
+              <Col>Singapore</Col>
+              <Col>
+                <Clock timeZone="Asia/Singapore" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>New York</Col>
+              <Col>
+                <Clock timeZone="America/New_York" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Maputo</Col>
+              <Col>
+                <Clock timeZone="America/New_York" />
+              </Col>
+            </Row>
+          </Container>
         </header>
       </div>
     );
